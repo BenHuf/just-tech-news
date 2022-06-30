ea6215a316a96c4a0d1b1abc7ff62aed20bdacdc
BenHuf <benthehuffman@gmail.com>
2022-06-30 18:48:36 -0400

    password verification added

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+//!!!!!!!!! CHANGED FORCE TO FALSE TO STOP DROPPING TABLE AT STARTUP !!!!!!!!!
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
